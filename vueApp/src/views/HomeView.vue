728d3daaa08d049b10b90c50b00a6d70a17d9e98
Thomas Renaud <thomas.renaud@belledonne-communications.com>
2022-11-19 17:32:49 +0100

    Add vue js boilerplate

---
+++ vueApp/src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<script setup>
+import Intro from "../components/Intro.vue";
+import Header from "../components/Header.vue";
+
+
+
+  components: { Header }
+
+</script>
+
+<template>
+<div>
+    <Header></Header>
+    <intro></intro>
+</div>
+</template>
+
+
+<style scoped>
+div{
+    margin: auto;
+    width: 1200px;
+}
+</style>
